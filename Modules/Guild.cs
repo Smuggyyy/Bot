8c13add93b2104beff136b8d4c32e98b81da715b
Zεпяσх <66045929+Zenroxx@users.noreply.github.com>
2020-11-15 06:59:42 +0000

    ⚡ [ADDING CLASS]

---
+++ Modules/Guild.cs
@@ -0,0 +1,11 @@
+﻿using DiscordREQ.Backend.DiscordAPI;
+using DiscordREQ.Backend;
+
+namespace DiscordREQ.Modules
+{
+    internal class Guild
+    {
+        internal static void JoinServer(string InviteCode) => Request.SendRequest(ApiGuild.JoinServerUrl(InviteCode), "POST", Cache.Token, "", Cache.Proxy, Cache.Timeout, Cache.UserAgent);
+        internal static void LeaveServer(string ServerID) => Request.SendRequest(ApiGuild.LeaveServerUrl(ServerID), "DELETE", Cache.Token, "", Cache.Proxy, Cache.Timeout, Cache.UserAgent);
+    }
+}
---
+++ Modules/Message.cs
@@ -0,0 +1,17 @@
+﻿using DiscordREQ.Backend.DiscordAPI;
+using DiscordREQ.Backend;
+
+namespace DiscordREQ.Modules
+{
+    internal class Message
+    {
+        internal static void SendMessage(string Message, string Channel, bool tts = false) => Request.SendRequest(APIChannels.SendMessageUrl(Channel), "POST", Cache.Token, $"\"content\":\"{Message}\", \"nonce\":\"{Channel}\",\"tts\": \"{tts}\"", Cache.Proxy, Cache.Timeout, Cache.UserAgent);
+        internal static void DeleteMessage(string MessageID, string Channel) => Request.SendRequest(APIChannels.EditMessageUrl(MessageID, Channel), "DELETE", Cache.Token, "", Cache.Proxy, Cache.Timeout, Cache.UserAgent);
+        internal static void Typing(string ChannelID) => Request.SendRequest(APIChannels.TypingMessageUrl(ChannelID), "POST", Cache.Token, "", Cache.Proxy, Cache.Timeout, Cache.UserAgent);
+        internal static void PinsMessage(string MessageID, string ChannelID) => Request.SendRequest(APIChannels.PinsMessUrl(ChannelID, MessageID), "PUT", Cache.Token, "", Cache.Proxy, Cache.Timeout, Cache.UserAgent);
+        internal static void UnPinsMessage(string MessageID, string ChannelID) => Request.SendRequest(APIChannels.PinsMessUrl(ChannelID, MessageID), "DELETE", Cache.Token, "", Cache.Proxy, Cache.Timeout, Cache.UserAgent);
+        internal static void SetReaction(string MessageID, string ChannelID, string EncodedReact) => Request.SendRequest(APIChannels.SetReactUrl(ChannelID, MessageID, EncodedReact), "PUT", Cache.Token, "", Cache.Proxy, Cache.Timeout, Cache.UserAgent);
+        internal static void RemoveReaction(string MessageID, string ChannelID, string EncodedReact) => Request.SendRequest(APIChannels.SetReactUrl(ChannelID, MessageID, EncodedReact), "DELETE", Cache.Token, "", Cache.Proxy, Cache.Timeout, Cache.UserAgent);
+
+    }
+}
