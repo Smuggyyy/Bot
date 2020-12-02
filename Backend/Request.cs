ef081553df215ccf22973706e9c235c5615ec531
_ ΖΞNRΘX #0001 <66045929+Zenroxx@users.noreply.github.com>
2020-12-02 22:15:17 +0100

    ⭐ 0.0.2 > Thread - Socks4, Socks5 - Fingerprint
    🚨 N'est possiblement pas actualisé avec les autres fichiers

--- Backend/Request.cs
+++ Backend/Request.cs
@@ -1,48 +1,90 @@
-﻿/*
+/*
  * Class : Request
  * Desc. : Faire très simplement une requête en 1 seul ligne, return TRUE si la reqûete c'est bien passer return FALSE si une erreur est survenue. La reponsse est stocker dans la variable LastedRequestResp
  * Author: Zenrox
- * Date  : 15/2020 - 18h
+ * Date  : 02/12/2020 - 21h
+ * Ajout : Thread, Support proxy socks4 & 5 | Proxy type & fingerprint personalisé
 */
 
-using System;
-using System.IO;
-using System.Net;
-using System.Text;
+using System.Threading;
 using DiscordREQ.Backend;
+using Leaf.xNet;
+using System;
 
 namespace DiscordREQ
 {
     internal class Request
     {
+        
         internal static bool SendRequest(string url, string Method, string Data = "")
         {
-            try
+            switch (Cache.Threaded)
             {
-                var Request = (HttpWebRequest)WebRequest.Create(url);
-                var RequestData = Encoding.ASCII.GetBytes("{" + Data + "}");
-
-                if (Cache.Proxy != "")
-                    Request.Proxy = new WebProxy(Cache.Proxy.Split(':')[0], Convert.ToInt32(Cache.Proxy.Split(':')[1]));
-
-                Request.Method = Method;
-                Request.Timeout = Cache.Timeout;
-                Request.UserAgent = Cache.UserAgent;
-                Request.ContentLength = RequestData.Length;
-                Request.ContentType = "application/json";
-                Request.Headers.Add("authorization", Cache.Token);
-
-                using (var stream = Request.GetRequestStream()) { stream.Write(RequestData, 0, RequestData.Length); }
-                var Response = new StreamReader(Request.GetResponse().GetResponseStream()).ReadToEnd();
-
-                Cache.CacheResponsse(Response);
-                return true;
+                case true:
+                    Thread Req = new Thread(() => PVSendRequest(url, Method, Data));
+                    Req.Start();
+                    return true;
+                case false:
+                    return PVSendRequest(url, Method, Data);
+                default:
+                    return PVSendRequest(url, Method, Data);
             }
-            catch (Exception e)
+        }
+
+    private static bool PVSendRequest(string url, string Method, string Data = "")
+        {
+            using (HttpRequest Request = new HttpRequest())
             {
-                Console.WriteLine(e.ToString());
-                Cache.LastedErrorMessage = e.ToString();
-                return false;
+                try
+                {
+                    switch (Cache.ProxyType)
+                    {
+                        case "HTTP":
+                            Request.Proxy = HttpProxyClient.Parse(Cache.Proxy);
+                            break;
+                        case "SOCKS4":
+                            Request.Proxy = Socks4ProxyClient.Parse(Cache.Proxy);
+                            break;
+                        case "SOCKS5":
+                            Request.Proxy = Socks5ProxyClient.Parse(Cache.Proxy);
+                            break;
+                        default:
+                            Request.Proxy = null;
+                            break;
+                    }
+
+                    Request.UserAgent = Cache.UserAgent;
+                    Request.IgnoreProtocolErrors = true;
+                    Request.ConnectTimeout = Cache.Timeout;
+                    Request.UseCookies = true;
+                    Request.KeepAlive = true;
+                    Request.AddHeader("Authorization", Cache.Token);
+                    Request.AddHeader("X-Fingerprint", Cache.XFingerPrint);
+
+                    switch (Method)
+                    {
+                        case "GET":
+                            Cache.CacheResponsse(Request.Get(url).ToString());
+                            break;
+                        case "PUT":
+                            Cache.CacheResponsse(Request.Put(url).ToString());
+                            break;
+                        case "DELETE":
+                            Cache.CacheResponsse(Request.Delete(url).ToString());
+                            break;
+                        case "PATCH":
+                            Cache.CacheResponsse(Request.Patch(url).ToString());
+                            break;
+                    }
+
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    Cache.LastedErrorMessage = e.ToString();
+                    return false;
+                }
             }
         }
     }
